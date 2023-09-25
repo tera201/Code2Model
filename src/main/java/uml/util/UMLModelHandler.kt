a32ac4c677cb663d08c386085089336e64bd631e
rnaryshkin <r.naryshkin99@gmail.com>
2023-09-25 19:55:40 +0300

    refactoring

--- src/main/java/uml/util/UMLModelHandler.kt
+++ src/main/java/uml/util/UMLModelHandler.kt
@@ -1,6 +1,5 @@
 package uml.util
 
-import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.databind.module.SimpleModule
 import org.eclipse.emf.common.util.EList
@@ -13,23 +12,37 @@ import java.io.IOException
 class UMLModelHandler {
     internal val objectMapper = ObjectMapper().apply {
         val module = SimpleModule()
+        applySerializers(module)
+        applyDeserializers(module)
+        registerModule(module)
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        applyMixin(this)
+    }
+
+    private fun applySerializers(module: SimpleModule) {
         module.addSerializer(ClassImpl::class.java, CustomClassSerializer())
             .addSerializer(InterfaceImpl::class.java, CustomInterfaceSerializer())
             .addSerializer(PackageImpl::class.java, CustomPackageSerializer())
-            .addDeserializer(EList::class.java, EListDeserializer())
+    }
+
+    private fun applyDeserializers(module: SimpleModule) {
+        module.addDeserializer(EList::class.java, EListDeserializer())
             .addDeserializer(ParameterImpl::class.java, ParameterDeserializer())
             .addDeserializer(PropertyImpl::class.java, PropertyDeserializer())
             .addDeserializer(GeneralizationImpl::class.java, GeneralizationDeserializer())
             .addDeserializer(InterfaceRealizationImpl::class.java, InterfaceRealizationDeserializer())
-        registerModule(module)
-        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        addMixIn(AssociationImpl::class.java, AssociationImplMixin::class.java)
-        addMixIn(PropertyImpl::class.java, PropertyImplMixin::class.java)
-        addMixIn(ParameterImpl::class.java, ParameterImplMixin::class.java)
-        addMixIn(OperationImpl::class.java, OperationImplMixin::class.java)
-        addMixIn(GeneralizationImpl::class.java, GeneralizationImplMixin::class.java)
-        addMixIn(InterfaceRealizationImpl::class.java, InterfaceRealizationImplMixin::class.java)
-        addMixIn(CommentImpl::class.java, CommentImplMixin::class.java)
+    }
+
+    private fun applyMixin(objectMapper: ObjectMapper) {
+        objectMapper.apply {
+            addMixIn(AssociationImpl::class.java, AssociationImplMixin::class.java)
+            addMixIn(PropertyImpl::class.java, PropertyImplMixin::class.java)
+            addMixIn(ParameterImpl::class.java, ParameterImplMixin::class.java)
+            addMixIn(OperationImpl::class.java, OperationImplMixin::class.java)
+            addMixIn(GeneralizationImpl::class.java, GeneralizationImplMixin::class.java)
+            addMixIn(InterfaceRealizationImpl::class.java, InterfaceRealizationImplMixin::class.java)
+            addMixIn(CommentImpl::class.java, CommentImplMixin::class.java)
+        }
     }
 
     fun addIndentation() {
@@ -42,18 +55,13 @@ class UMLModelHandler {
             objectMapper.writeValue(File(filename), umlModel)
         } catch (e: IOException) {
             e.printStackTrace()
-        } catch (e: JsonProcessingException) {
-            e.printStackTrace()
         }
     }
 
-    internal inline fun <reified T> loadModelFromFile(filename: String): T? {
+    fun loadModelFromFile(filename: String): ModelImpl? {
         return try {
-            objectMapper.readValue(File(filename), T::class.java)
+            objectMapper.readValue(File(filename), ModelImpl::class.java)
         } catch (e: IOException) {
-            e.printStackTrace()
-            return null
-        } catch (e: JsonProcessingException) {
             e.printStackTrace()
             return null
         }
