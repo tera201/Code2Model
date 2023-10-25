2ec8149bdd5273f4ae5efd6399c0c6f45d462725
rnaryshkin <r.naryshkin99@gmail.com>
2023-10-25 17:38:35 +0300

    update

--- src/main/java/uml/helpers/DataUMLBuilderClass.kt
+++ src/main/java/uml/helpers/DataUMLBuilderClass.kt
@@ -3,14 +3,14 @@ package uml.helpers
 data class BuilderClass (
     val imports: BuilderImports,
     val name: String,
-    val isAbstract: Boolean?,
-    val isStatic: Boolean?,
-    val isFinal: Boolean?,
-    val modifier: String?,
-    val extend: String?,
+    val modifiers: BuilderModifiers,
+    val parentName: String?,
     val interfaceList: List<String>?,
     val isNested: Boolean,
-)
+) {
+    constructor(name: String, isAbstract: Boolean, isNested: Boolean) : this(BuilderImports(), name, BuilderModifiers(isAbstract), null, null, isNested)
+    constructor(name: String, isAbstract: Boolean, parentName:String?, isNested: Boolean) : this(BuilderImports(), name, BuilderModifiers(isAbstract), parentName, null, isNested)
+}
 
 data class BuilderImports (
     val elementImports: List<String> = ArrayList(),
@@ -18,3 +18,12 @@ data class BuilderImports (
     val staticElementImports: List<String> = ArrayList(),
     val staticPackageImports: List<String> = ArrayList(),
 )
+
+data class BuilderModifiers(
+    val isAbstract: Boolean,
+    val isStatic: Boolean,
+    val isFinal: Boolean,
+    val visibility: String?
+) {
+    constructor(isAbstract: Boolean) : this(isAbstract, false, false, null)
+}
