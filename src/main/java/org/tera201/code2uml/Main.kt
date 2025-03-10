4cdf1a094c20cd7e8b15d0da35eac13c3904a443
rnaryshkin <r.naryshkin99@gmail.com>
2025-03-10 23:09:17 +0000

    Updated AnalyzerBuilder

---
+++ src/main/java/org/tera201/code2uml/Main.kt
@@ -0,0 +1,17 @@
+package org.tera201.code2uml
+
+import org.tera201.code2uml.util.messages.DataBaseUtil
+import java.io.File
+
+fun main() {
+    val projectPath = "."
+    val projectDir = File(projectPath).canonicalFile.toString()
+    val dbUrl = "$projectDir/Samples/db/model.db"
+    val dataBaseUtil = DataBaseUtil(dbUrl)
+    val analyzer = AnalyzerBuilder("Main", "v1")
+        .setLanguage(Language.Kotlin, )
+        .setPath("$projectDir/Samples/kotlin")
+        .setDataBaseUtil(dataBaseUtil)
+        .threads(4)
+    analyzer.buildDB()
+}
